2c3db85293846d52f1b74b7f05844c71bdd7de26
Shane Unruh <shane.unruh@hpe.com>
2025-12-18 13:04:06 -0700

    Improved test_smd_compare but is still not checking results
    Signed-off-by: Shane Unruh <shane.unruh@hpe.com>

--- tests/pytests/test_smd_compare.py
+++ tests/pytests/test_smd_compare.py
@@ -4,13 +4,19 @@
 
 import pytest
 import requests
+import json
+
+smd_base_url = "http://smd:27779/hsm"
+smd2_base_url = "http://smd2:8080/hsm"
 
 @pytest.fixture()
 def discover_hardware():
     # setup
-    bmc_nodes = [ "x0c0s1b0", "x0c0s2b0", "x0c0s3b0", "x0c0s4b0" ]
 
-    smd_url = "http://smd:27779/hsm/v2/Inventory/RedfishEndpoints"
+    headers = {
+    }
+
+    bmc_nodes = [ "x0c0s1b0", "x0c0s2b0", "x0c0s3b0", "x0c0s4b0" ]
     for node in bmc_nodes:
         print(f"discover: {node}")
         request_body = {
@@ -23,10 +29,25 @@ def discover_hardware():
                      "Password" : "root_password"
                      }]
                 }
-        response = requests.post(smd_url, json=request_body)
-        print(f"Status code: {response.status_code}")
-        print("Response: ")
+        response = requests.post(f"{smd_base_url}/v2/Inventory/RedfishEndpoints", json=request_body)
+        print(f"SMD POST RedfishEndpoints: Status code: {response.status_code}")
+        print("SMD POST RedfishEndpoints Response: ")
         print(response.json())
+
+    print("POST Components to SMD2")
+    response = requests.get(f"{smd_base_url}/v2/State/Components")
+    print(f"SMD GET Components: Status code: {response.status_code}")
+    print("SMD GET Components Response: ")
+    print(response.text)
+    smd_components = json.loads(response.text)
+
+    # post components to smd2
+    response = requests.post(f"{smd2_base_url}/v2/State/Components", json=smd_components)
+    print(f"SMD2 POST Components: Status code: {response.status_code}")
+    print("SDM2 POST Components Response: ")
+    print(response.text)
+
+    # assert False
 
     yield
 
