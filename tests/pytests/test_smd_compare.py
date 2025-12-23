7d48e5c7a296c0d4ad37ff856092bb04e894155a
Shane Unruh <shane.unruh@hpe.com>
2025-12-22 17:25:46 -0700

    Improved the pytests
    - Added a comparison of the Components
    - Added a wait for node discovery
    - Changed the test image to run pytest by default
    
    Signed-off-by: Shane Unruh <shane.unruh@hpe.com>

--- tests/pytests/test_smd_compare.py
+++ tests/pytests/test_smd_compare.py
@@ -5,6 +5,9 @@
 import pytest
 import requests
 import json
+import time
+import pprint
+from deepdiff import DeepDiff
 
 smd_base_url = "http://smd:27779/hsm"
 smd2_base_url = "http://smd2:8080/hsm"
@@ -13,11 +16,29 @@ smd2_base_url = "http://smd2:8080/hsm"
 def discover_hardware():
     # setup
 
+    smd_base_url = "http://smd:27779/hsm"
+    smd2_base_url = "http://smd2:8080/hsm"
+
     headers = {
     }
 
     bmc_nodes = [ "x0c0s1b0", "x0c0s2b0", "x0c0s3b0", "x0c0s4b0" ]
-    for node in bmc_nodes:
+
+    response = requests.get(f"{smd_base_url}/v2/Inventory/RedfishEndpoints")
+    if not response.ok:
+        print_response("GET", response)
+        pytest.fail(f"Failed to get {response.url}")
+
+    discovered_nodes = [ endpoint.get("ID")
+                        for endpoint in json.loads(response.text).get("RedfishEndpoints", [])
+                        if endpoint.get("DiscoveryInfo", {}).get("LastDiscoveryStatus") == "DiscoverOK"]
+    undiscovered_nodes = list(set(bmc_nodes) - set(discovered_nodes))
+    print(f"bmc_nodes: {bmc_nodes}")
+    print(f"discovered_nodes: {discovered_nodes}")
+    print(f"undiscovered_nodes: {undiscovered_nodes}")
+
+
+    for node in undiscovered_nodes:
         print(f"discover: {node}")
         request_body = {
                 "RedfishEndpoints" : [
@@ -30,24 +51,39 @@ def discover_hardware():
                      }]
                 }
         response = requests.post(f"{smd_base_url}/v2/Inventory/RedfishEndpoints", json=request_body)
-        print(f"SMD POST RedfishEndpoints: Status code: {response.status_code}")
-        print("SMD POST RedfishEndpoints Response: ")
-        print(response.json())
+        if not response.ok:
+            print_response("POST", response)
 
-    print("POST Components to SMD2")
+    if undiscovered_nodes:
+        for i in range(0, 10):
+            done = True
+            print(f"Waiting for discovery to finish. {i}")
+            response = requests.get(f"{smd_base_url}/v2/Inventory/RedfishEndpoints")
+            if response.ok:
+                endpoints = json.loads(response.text)
+                discovery_results = { endpoint.get("ID"): endpoint.get("DiscoveryInfo").get("LastDiscoveryStatus")
+                                      for endpoint in endpoints.get("RedfishEndpoints")}
+                pprint.pprint(discovery_results)
+                for node in undiscovered_nodes:
+                    endpoint = discovery_results.get(node)
+                    print(f"{node} {endpoint}")
+                    if endpoint != "DiscoverOK":
+                        print(f"- {node} {endpoint}")
+                        done = False
+                if done:
+                    break
+            time.sleep(1)
+
     response = requests.get(f"{smd_base_url}/v2/State/Components")
-    print(f"SMD GET Components: Status code: {response.status_code}")
-    print("SMD GET Components Response: ")
-    print(response.text)
+    if not response.ok:
+        print_response("GET", response)
     smd_components = json.loads(response.text)
 
-    # post components to smd2
+    print("POST Components to SMD2")
     response = requests.post(f"{smd2_base_url}/v2/State/Components", json=smd_components)
-    print(f"SMD2 POST Components: Status code: {response.status_code}")
-    print("SDM2 POST Components Response: ")
-    print(response.text)
+    if not response.ok:
+        print_response("POST", response)
 
-    # assert False
 
     yield
 
@@ -55,5 +91,46 @@ def discover_hardware():
 
 
 def test_compare(discover_hardware):
-    print("compare")
+    response = requests.get(f"{smd_base_url}/v2/State/Components")
+    if response.status_code != 200:
+        print_response("GET", response)
+        pytest.fail(f" get {response.url}, code: {response.status_code}")
 
+    smd_components = json.loads(response.text)
+
+    response = requests.get(f"{smd2_base_url}/v2/State/Components")
+    if not response.ok:
+        print_response("GET", response)
+        pytest.fail(f"get {response.url}, code: {response.status_code}")
+
+    smd2_components = json.loads(response.text)
+
+    diff = compare(smd_components.get("Components"), smd2_components.get("Components"))
+    if diff:
+        pytest.fail(f"The Component list from SMD does not match the list from SMD2. diff: {diff}")
+
+
+def get_discovered_nodes(redfishEndpoints):
+    discovered_nodes = [ endpoint.get("ID")
+                        for endpoint in redfishEndpoints.get("RedfishEndpoints", [])
+                        if endpoint.get("DiscoveryInfo", {}).get("LastDiscoveryStatus") == "DiscoveryOK"]
+
+def compare(expected, actual):
+
+    print("---")
+    print(json.dumps(expected, indent=4))
+    print("---")
+    print(json.dumps(actual, indent=4))
+
+    diff = DeepDiff(expected, actual, group_by="ID")
+    print("---")
+    print(diff)
+    print("---")
+
+    return diff
+
+
+def print_response(method, response):
+        print(f"{method} URL: {response.url}, Code: {response.status_code}, Body:")
+        print(response.text)
+        print(json.dumps(response.text, indent=4))
